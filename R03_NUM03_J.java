34f0e45e5484d33d6567c59ebf51a20f02b424d5
Rhea Nibert <rheanibert@MacBook-Air.local>
2025-02-24 02:00:08 -0500

    Initial upload of R03_NUM03_J.java - Noncompliant example

---
+++ R03_NUM03_J.java
@@ -0,0 +1,8 @@
+// Rule 03. Numeric Types and Operations (NUM)
+// NUM03-J. Use integer types that can fully represent the possible range of unsigned data
+
+// NONCOMPLIANT EXAMPLE
+
+public static int getInteger(DataInputStream is) throws IOException {
+  return is.readInt(); 
+}

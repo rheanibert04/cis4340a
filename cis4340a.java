c4516a57e60e727a36bac6492d6ce2d43caf3240
Rhea Nibert <rheanibert@MacBook-Air.local>
2025-02-23 23:58:06 -0500

    Added file cis4340a.java to edits branch

---
+++ cis4340a.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *  Compilation:  javac cis4340a.java
+ *  Execution:    java cis4340a
+ *
+ *  Prints 
+ *    "Hello, World - from Rhea Nibert - Spring 2024 - UCF.
+ *  By tradition, this is everyone's first program.
+ *
+ * Revision History:
+ *   % java Cis4615a
+ *  Hello, World - from the class of CIS4615 - Fall 2022 - UCF
+ *  %
+ *
+ ******************************************************************************/
+
+public class cis4340a {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - from Rhea Nibert - Spring 2024 - UCF");
+    }
+
+}

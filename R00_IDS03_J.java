5024d617d2c096089bd2028e16986f949cc2a63e
Rhea Nibert <rheanibert@MacBook-Air.local>
2025-02-24 00:08:11 -0500

    Fixed upload of R00_IDS03_J.java - Compliant solution

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,11 +1,16 @@
 // Rule 00. Input Validation and Data Sanitization (IDS)
 // IDS03-J. Do not log unsanitized user input
 
-// NONCOMPLIANT EXAMPLE
+// COMPLIANT SOLUTION
 
 if (loginSuccessful) {
-  logger.severe("User login succeeded for: " + username);
+  logger.severe("User login succeeded for: " + sanitizeUser(username));
 } else {
-  logger.severe("User login failed for: " + username);
+  logger.severe("User login failed for: " + sanitizeUser(username));
+}
+
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
 }
 

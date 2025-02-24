6bc4885cc5189273cf09185747647f76f6f0c5cc
Rhea Nibert <rheanibert@MacBook-Air.local>
2025-02-24 00:05:23 -0500

    Initial upload of R00_IDS03_J.java - Noncompliant example

---
+++ R00_IDS03_J.java
@@ -0,0 +1,11 @@
+// Rule 00. Input Validation and Data Sanitization (IDS)
+// IDS03-J. Do not log unsanitized user input
+
+// NONCOMPLIANT EXAMPLE
+
+if (loginSuccessful) {
+  logger.severe("User login succeeded for: " + username);
+} else {
+  logger.severe("User login failed for: " + username);
+}
+

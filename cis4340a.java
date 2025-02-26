18f1ad1e7190fdda5b05ee4ea4e9f47a84a38bba
Rhea Nibert <rheanibert@MacBook-Air.local>
2025-02-25 23:22:33 -0500

    Updated cis4340a.java

--- cis4340a.java
+++ cis4340a.java
@@ -3,12 +3,12 @@
  *  Execution:    java cis4340a
  *
  *  Prints 
- *    "Hello, World - from Rhea Nibert - Spring 2024 - UCF.
+ *    "Hello, World - from Rhea Nibert - Spring 2024 - UCF".
  *  By tradition, this is everyone's first program.
  *
  * Revision History:
- *   % java Cis4615a
- *  Hello, World - from the class of CIS4615 - Fall 2022 - UCF
+ *   % java Cis4340
+ *  Hello, World - from the class of cis4340 - Spring 2024 - UCF (your name here)
  *  %
  *
  ******************************************************************************/
